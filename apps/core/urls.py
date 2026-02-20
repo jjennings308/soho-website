faddb8ce6b76fc94c95b76905a07ac1eb983ee17
James Jennings <jjennings308@gmail.com>
2026-02-20 13:31:11 -0500

    More tuning

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,8 +4,7 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('home', views.home, name='home'),
+    path('', views.home, name='home'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
 ]
