42b88aecd0b88397119fff5c1d1a40b37fa113db
James Jennings <jjennings308@gmail.com>
2026-03-27 11:19:38 -0400

    change to Postgres for db

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,11 +6,24 @@ from .base import *
 DEBUG = False
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
+'''
 # Database - SQLite
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', default='soho_db'),
+        'USER': config('DB_USER', default='soho_user'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
